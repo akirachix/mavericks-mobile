864e7a824f9ecb6e95d50a0f690e682fdaaf7948
Meaza Misgina <misginameaza@gmail.com>
2025-07-07 10:08:09 +0300

    fix: Fixed merge conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ android {
     buildFeatures {
         compose = true
     }
-
 }
 
 dependencies {
@@ -69,5 +68,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.8.1")
+
+    implementation(libs.androidx.navigation.compose)
 
 }
--- app/src/main/java/com/mavericks/mitumbaesales/ui/theme/Theme.kt
+++ app/src/main/java/com/mavericks/mitumbaesales/ui/theme/Theme.kt
@@ -1,7 +1,12 @@
 package com.mavericks.miumbaseales.ui.themes
-
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
@@ -14,6 +19,14 @@ import com.mavericks.mitumbaesales.R
 import com.mavericks.mitumbaesales.ui.theme.Beige
 import com.mavericks.mitumbaesales.ui.theme.Gold
 import com.mavericks.mitumbaesales.ui.theme.NavyBlue
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import com.mavericks.mitumbaesales.ui.theme.GoldenrodYellow
+import com.mavericks.mitumbaesales.ui.theme.LightYellow
+import com.mavericks.mitumbaesales.ui.theme.White
 
 
 val Poppins = FontFamily(
@@ -22,6 +35,21 @@ val Poppins = FontFamily(
     Font(R.font.poppins_light, FontWeight.Light)
 )
 
+
+
+
+private val DarkColorScheme = darkColorScheme(
+    primary = NavyBlue,
+    onPrimary = White,
+    secondary = GoldenrodYellow,
+    onSecondary = White,
+    surface = Color(0xFF1C1B1F),
+    onSurface = Color(0xFFE6E1E5),
+    background = Color(0xFF1C1B1F),
+    onBackground = Color(0xFFE6E1E5),
+    surfaceVariant = NavyBlue,
+    onSurfaceVariant = White
+)
 
 val AppTypography = Typography(
     titleLarge = TextStyle(
@@ -47,11 +75,47 @@ val AppTypography = Typography(
 
 private val LightColorScheme = lightColorScheme(
     primary = NavyBlue,
-    secondary = Gold,
-    surface = Beige,
-    onPrimary = Color.White,
-    onSecondary = Color.Black
+    onPrimary = White,
+    secondary = GoldenrodYellow,
+    onSecondary = White,
+    surface = White,
+    onSurface = NavyBlue,
+    background = White,
+    onBackground = NavyBlue,
+    surfaceVariant = LightYellow,
 )
+
+@Composable
+fun MitumbaesalesTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        content = content
+    )
+}
+
 
 @Composable
 fun AppTheme(
