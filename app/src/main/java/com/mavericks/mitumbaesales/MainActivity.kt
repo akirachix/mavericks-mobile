453c4a7ff717ec81d415df7fe773a52949b9cbf3
wangara <mercywangarakwagalakwe@gmail.com>
2025-07-16 08:28:30 +0300

    Made fonts to match the design

--- app/src/main/java/com/mavericks/mitumbaesales/MainActivity.kt
+++ app/src/main/java/com/mavericks/mitumbaesales/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MitumbaesalesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SignUpScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MitumbaesalesTheme {
-        Greeting("Android")
-    }
-}
